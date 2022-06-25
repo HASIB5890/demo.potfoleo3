af5e3815a70f290a4ee8232ada11cd11a024b19e
Md Hasib Howlader <85785402+hasib5890@users.noreply.github.com>
2022-06-26 01:04:04 +0600

    demo3 oponal

---
+++ index.js
@@ -0,0 +1,25 @@
+let usa= new Typed(".demo",{
+    strings : ["Roni howlader","Abir howlader","fontandevolapar"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+})
+let photo=['img1.jpg','img2.png','img3.png','img4.png']
+let chan=document.querySelector(".img1g");
+let count=0;
+function changeimg(){
+count++;
+if(count>=photo.length){
+    count=0;
+    chan.src=photo[count];
+}
+   else{
+    chan.src=photo[count];
+   } 
+
+
+}
+function color(){
+alert("Hasib succes value")
+  
+}
